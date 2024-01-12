57111b9ddaf07863b753e8e8cdafb76f199b819a
JorikFat <jorikfat@gmail.com>
2024-01-12 03:27:54 +0300

    separate network client

--- app/src/main/java/dev/jorik/test/bybit/App.kt
+++ app/src/main/java/dev/jorik/test/bybit/App.kt
@@ -1,6 +1,7 @@
 package dev.jorik.test.bybit
 
 import android.app.Application
+import io.ktor.client.HttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -12,8 +13,11 @@ class App : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
+            module {
+            }
             modules(module{
-                single<MainRepository.Network> { MainNetwork() }
+                single<HttpClient> { networkClient }
+                single<MainRepository.Network> { MainNetwork(get()) }
                 single<MainDomain> { MainRepository(get()) }
                 viewModel { MainViewModel(get()) }
             })
--- app/src/main/java/dev/jorik/test/bybit/MainNetwork.kt
+++ app/src/main/java/dev/jorik/test/bybit/MainNetwork.kt
@@ -1,32 +1,17 @@
 package dev.jorik.test.bybit
 
 import io.ktor.client.HttpClient
-import io.ktor.client.features.json.GsonSerializer
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.logging.DEFAULT
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logger
-import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 
-class MainNetwork(): MainRepository.Network {
-    private val client = HttpClient(){
-        install(JsonFeature) {
-            serializer = GsonSerializer()
-        }
-        install(Logging) {
-            logger = Logger.DEFAULT
-            level = LogLevel.ALL
-        }
-    }
+class MainNetwork(
+    private val client: HttpClient
+) : MainRepository.Network {
+    private val endpoint = "https://api.bybit.com/v5/announcements/index"
 
-    val endpoint = "https://api.bybit.com/v5/announcements/index"
-
-    override suspend fun getItems(): Response {
-        return client.get(endpoint){
+    override suspend fun getItems(): Response =
+        client.get(endpoint) {
             parameter("locale", "en-US")
             parameter("limit", 10)
         }
-    }
 }
