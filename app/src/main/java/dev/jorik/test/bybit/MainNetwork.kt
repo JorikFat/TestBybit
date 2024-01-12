7ee0fe57d68b7870bd5a1c95f1764b24e2e50893
JorikFat <jorikfat@gmail.com>
2024-01-12 03:17:00 +0300

    add ktor

--- app/src/main/java/dev/jorik/test/bybit/MainNetwork.kt
+++ app/src/main/java/dev/jorik/test/bybit/MainNetwork.kt
@@ -1,12 +1,32 @@
 package dev.jorik.test.bybit
 
-import com.google.gson.Gson
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 
-class MainNetwork(
-    //todo add client
-): MainRepository.Network {
+class MainNetwork(): MainRepository.Network {
+    private val client = HttpClient(){
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
+        }
+    }
+
+    val endpoint = "https://api.bybit.com/v5/announcements/index"
 
     override suspend fun getItems(): Response {
-        return Gson().fromJson(stubJson, Response::class.java)
+        return client.get(endpoint){
+            parameter("locale", "en-US")
+            parameter("limit", 10)
+        }
     }
 }
